3f7ab57b974bb39181372e227843757c75137456
Mubarak <testaccforcolab@gmail.com>
2024-09-14 17:34:35 +0530

    added slow loding tables

--- text2sql.py
+++ text2sql.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI, HTTPException, Query, Form,Request
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import os
-import sys
 # Initialize the OpenAI client
 api_key=os.environ.get('OPENAI_API_KEY')
 client = OpenAI(api_key=api_key)
@@ -125,7 +124,8 @@ assistant = client.beta.assistants.create(
                     for a successful execution True is returned as result.
      
                     after the sql query executed  an output is generated for a end user summarizing the details . you can only reveal you identity as a data Analysis ai an you help in fetching and getting insights from data.
-                    
+                     
+                    do not include sql statements in text response only send them using tool call with execute_sql_query 
                     if the response to question has points and lists format the text example using  bullet points.
 
       ''',
@@ -245,7 +245,7 @@ async def send_prompt(request: Request ):
         # Access the 'prompt' key directly
         prompt = body.get("prompt")
 
-        if len(sys.argv)>1:
+        if prompt=='test':
             text={'type':'text','data':'Here is the information that you asked'}
             table={"type":"table","data":{"columns":["Name","Age","City","Occupation","Salary"],"rows":[["John Doe",28,"New York","Engineer",70000],["Jane Smith",34,"Los Angeles","Designer",85000],["Michael Brown",42,"Chicago","Manager",95000],["Emily Davis",29,"Houston","Developer",72000],["David Wilson",37,"Miami","Consultant",88000],["Sarah Johnson",31,"San Francisco","Architect",93000],["Chris Lee",45,"Seattle","Scientist",100000],["Jessica Taylor",26,"Boston","Data Analyst",68000],["James Martin",40,"Denver","Lawyer",110000],["Laura Garcia",33,"Phoenix","Doctor",120000]]}}
         
