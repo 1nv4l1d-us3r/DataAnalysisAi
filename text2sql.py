5d8c3ee961ce866cc05752f8b7fc2361725fa5d9
Mubarak <testaccforcolab@gmail.com>
2024-09-21 15:08:20 +0530

    tidy up

--- text2sql.py
+++ text2sql.py
@@ -12,42 +12,11 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import os
 import re
+from dbConnection import getConnection
+
 # Initialize the OpenAI client
 api_key=os.environ.get('OPENAI_API_KEY')
 client = OpenAI(api_key=api_key)
-
-
-username=os.environ.get('SNOWFLAKE_USERNAME')
-password=os.environ.get('SNOWFLAKE_PASSWORD')
-account=os.environ.get('SNOWFLAKE_ACCOUNT')
-database=os.environ.get('SNOWFLAKE_DATABASE')
-warehouse=os.environ.get('SNOWFLAKE_WAREHOUSE')
-
-db_details={
-    'user':username,
-    'password':password,
-    'account':account,
-    'database':database,
-    'warehouse':warehouse,
-    'schema':'DATA'
-    }
-
-
-connection=None
-
-def getConnection():
-    global connection
-
-    if connection:
-        if not connection.is_closed():
-            return connection
-        
-    print('creating connection to snowflake')
-    connstart=time.time()
-    conn = snowflake.connector.connect(**db_details)
-    connection=conn
-    print('connection took',round(time.time()-connstart,2),'s')
-    return conn
 
 connection=getConnection()
 class EventHandler(AssistantEventHandler):
